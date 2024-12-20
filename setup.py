553253aa5061838d266a6b7eaa79b3ed7c72b60e
hello madhu <kuruvamadhusekhar76@gmail.com>
2024-12-20 17:00:24 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='kms',
+    author_email = 'madhupranitha96@gmail.com',
+    install_requirements = [],
+    packages = find_packages(),
+
+)
